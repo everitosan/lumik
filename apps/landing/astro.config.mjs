82903e5c6490b2ec195b5531f41478dcb0966bba
everardo <everardo.ipn@gmail.com>
2026-06-19 10:21:47 -0600

    feat: es lang for landing

--- apps/landing/astro.config.mjs
+++ apps/landing/astro.config.mjs
@@ -2,4 +2,11 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://lumik.evesan.rocks',
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en', 'es'],
+    routing: {
+      prefixDefaultLocale: false,
+    },
+  },
 });
